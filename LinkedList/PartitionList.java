6575daecba222768bd059428a5394e7080503cfe
coderchengg <zhicheng_ren@163.com>
2017-06-10 15:52:06 +0800

    LinkedListCode

---
+++ LinkedList/PartitionList.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head == null || head.next == null)  return head;
+        ListNode left_dummy = new ListNode(0);
+        ListNode right_dummy = new ListNode(0);
+        
+        ListNode left_cur = left_dummy;
+        ListNode right_cur = right_dummy;
+        
+        for(ListNode cur = head; cur != null; cur = cur.next){
+            if(cur.val < x){
+                left_cur.next = cur;
+                left_cur = cur;
+            }else{
+                right_cur.next = cur;
+                right_cur = cur;
+            }
+            
+          
+        }
+        left_cur.next = right_dummy.next;
+        right_cur.next = null;
+        return left_dummy.next;
+        
+    }
+}
